ec1ff848b314de6b9dc767ff318c3c26e062a9dd
alexk <alex.krasnov42@gmail.com>
2023-05-19 15:22:03 +0300

    сделано модальное окно выбора схемы

---
+++ Models/OutputModel/SchemaSelect.cs
@@ -0,0 +1,9 @@
+﻿namespace HIVBackend.Models.OutputModel
+{
+    public class SchemaSelect
+    {
+        public int Id { get; set; }
+        public string MedicineName { get; set; }
+        public int[] SchemaId { get; set; }
+    }
+}
