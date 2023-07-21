8118857ef59edf69028cbc1d482636b10a28c9a1
alexk <alex.krasnov42@gmail.com>
2023-07-21 15:06:16 +0300

    поиск иногородние

---
+++ Models/OutputModel/SearchNonresident.cs
@@ -0,0 +1,15 @@
+﻿namespace HIVBackend.Models.OutputModel
+{
+    public class SearchNonresident
+    {
+        public List<string>? ListSex { get; set; }
+        public List<string>? ListRegion { get; set; }
+        public List<string>? ListRegionPreset { get; set; }
+        public List<string>? ListCountry { get; set; }
+        public List<string>? ListStage { get; set; }
+        public List<Selector2Col>? ListCheckCourse { get; set; }
+        public List<string>? ListInfectCourse { get; set; }
+        public List<string>? ListShowIllness { get; set; }
+        public List<string>? ListYNA { get; set; }
+    }
+}
