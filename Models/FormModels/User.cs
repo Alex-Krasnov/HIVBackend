cbb91c7ee1204078e2ef929ad08c76cc3935eb0f
alexk <alex.krasnov42@gmail.com>
2024-03-20 14:56:57 +0300

    редактирование пользователей

---
+++ Models/FormModels/User.cs
@@ -0,0 +1,14 @@
+﻿namespace HIVBackend.Models.FormModels
+{
+    public class User
+    {
+        public string Uid { get; set; } = null!;
+        public string Pwd { get; set; } = null!;
+        public string? UserName { get; set; }
+        public bool Excel { get; set; }
+        public bool Write { get; set; }
+        public bool Admin { get; set; }
+        public bool Deleter { get; set; }
+        public bool Klassif { get; set; }
+    }
+}
