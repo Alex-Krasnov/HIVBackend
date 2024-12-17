ebcdb384bbb8f11b371238fc48957168c3cf9092
Alex Krasnov <alex.krasnov42@gmail.com>
2024-12-17 17:25:27 +0300

    +

--- Controllers/Search/SearchTreatmentController.cs
+++ Controllers/Search/SearchTreatmentController.cs
@@ -1,7 +1,5 @@
 ﻿using HIVBackend.Models.FormModels.Search;
 using HIVBackend.Models.OutputModel.Search;
-using HIVBackend.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HIVBackend.Controllers.Search
@@ -10,33 +8,5 @@ namespace HIVBackend.Controllers.Search
     [ApiController]
     public class SearchTreatmentController : SearchBaseController<SearchTreatmentForm, SearchTreatmentInputForm>
     {
-        //[HttpPost, Route("GetRes")]
-        //[Authorize(Roles = "User")]
-        //public IActionResult GetRes(SearchTreatmentInputForm form)
-        //{
-        //    int pageSize = 100;
-
-        //    // наполняем columName selectGroupSrt joinStr whereStr для дальнейшего поиска
-        //    form.SetSearchData();
-
-        //    if (form.Excel)
-        //    {
-        //        string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-        //        byte[] fileBytes = SerarchResService.GetExcelRes(authHeader: Request.Headers["Authorization"].First(),
-        //                                                         selectGroupSrt: form.selectGroupSrt,
-        //                                                         joinStr: form.joinStr,
-        //                                                         whereStr: form.whereStr,
-        //                                                         pageSize: pageSize,
-        //                                                         page: form.Page,
-        //                                                         columName: form.columName);
-
-        //        return File(fileBytes, contentType, "res_search.xlsx");
-        //    }
-
-        //    var searchRes = SerarchResService.GetSearchRes(form.selectGroupSrt, form.joinStr, form.whereStr, pageSize, form.Page);
-
-        //    searchRes.ColumName = form.columName;
-        //    return Ok(searchRes);
-        //}
     }
 }
